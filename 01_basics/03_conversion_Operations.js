4a4da17606831617cefef08cdda732cdd5801087
AMAN3-ENG <68887438+AMAN3-ENG@users.noreply.github.com>
2025-02-22 07:46:35 +0000

    compa

---
+++ 01_basics/03_conversion_Operations.js
@@ -0,0 +1,19 @@
+//  let score = 23
+
+//  console.log(typeof score);
+//  console.log(typeof(score));
+
+//  let valueInNumber = Number(score)
+//  console.log(typeof valueInNumber);
+//  console.log(valueInNumber)
+
+ let isLoggedIn
+
+let str1 = "hello"
+let str2 = "hitesh"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
